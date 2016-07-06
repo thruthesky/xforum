0269dae893d66377072357f893f01251cf0331c9
jeraisy21 <thruthesky@gmail.com>
2016-07-06 21:23:16 +0900

    work for its

--- template/its/list.php
+++ template/its/list.php
@@ -17,50 +17,6 @@ get_header();
 
 ?>
 
-<pre>
-admin ( manage member, issue, everything ),
-member,
-    => level 0~9.
-project,
-    => forum category is the project.
-    => forum category admin is the managers of the project.
-
-    => forum members are the members of its.
-    => forum admins are the its admin who can do anything to category.
-
-    => project statistics. ( who have many works. who have done many works )
-    =>
-    => issues,
-        => Issue information:
-            => worker ( who is working on this issue? )
-            => in-charge ( who will be responsible for this work? )
-            => dead-line
-        => issue status:
-            => work priority 1~9( is it urgent? ) never mind, low, medium, high, immediate, critical(don't do anything before this).
-            => work difficulty : very low, medium, high, very high.
-            => work process ( where is the work or when it will be finished? ). proposal, work started, in-process(how many percent it's done), alpha, beta(test), finished(wait for approval), done(approved), rejected( not-approved. do it again )
-            => work evaluation ( how is the work? ) : buggy, good, bad(work is not good. Maybe the source code is too complicated. ),
-
-How it work:
-=> if admin want to something that project manager can, then he must become project manager also.
-=> There may be more than 1 manager.
-=> close ( is the work finished and finalized? ). Only project manager can close the issue.
-=> open : anyone can open.
-=> Only worker, in-charge and manager can change the work status.
-=> Only manager can change issue info.
-
-Search & Sort :
-    => Search all project or some / one project.
-    => Memo upto 10 search option. like; over due ( by workers/in-charge ), list critical, list dead-line within a week
-    => member by workers, in-charge,
-    => deadline ( today, within a week, within a month, over due, over one week, over one month )
-    => issue information( worker, in-charge, dead-line)
-    => issue status( priority, difficulty, process, evaluation )
-    => date of issue ( recently updated, recently opened/created, oldest opened, new comment )
-
-</pre>
-
-
 
 
 
