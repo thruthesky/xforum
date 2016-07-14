d149d8f72ed6a110c94192984f68f46b56e383e3
eclarinalviel <eclarinalviel@gmail.com>
2016-07-14 19:23:16 +0800

    work for autolink

--- template/its/init.php
+++ template/its/init.php
@@ -67,33 +67,7 @@ class its {
         return false;
     }
 
-//    public function autoLink($text){
-//        // force http: on www.
-//        $text = ereg_replace( "www\.", "http://www.", $text );
-//        // eliminate duplicates after force
-//        $text = ereg_replace( "http://http://www\.", "http://www.", $text );
-//        $text = ereg_replace( "https://http://www\.", "https://www.", $text );
-//
-//        // The Regular Expression filter
-//        $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-//        // Check if there is a url in the text
-//        if(preg_match($reg_exUrl, $text, $url)) {
-//            // make the urls hyper links
-//            $text = preg_replace($reg_exUrl, '<a href="'.$url[0].'" rel="nofollow">'.$url[0].'</a>', $text);
-//        }    // if no urls in the text just return the text
-//
-//        return ($text);
-//    }
-//    public static function get_link_url() {
-//        $content = get_the_content();
-//        $has_url = get_url_in_content( $content );
-//
-//        return ( $has_url ) ? $has_url : apply_filters( 'the_permalink', get_permalink() );
-//    }
-
-
-
-
+    
 }
 
 
